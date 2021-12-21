ff1c8a6e38b67c83f99e89c3d097a0f6152bd539
bvedant19 <71204063+bvedant19@users.noreply.github.com>
2021-12-22 01:36:30 +0530

    initial commit

---
+++ build/precache-manifest.313621e85d7179a4a40d414a1168aff7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f050a8b5b75f014a6160fb95a8878f3c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8ef7b5592a0842813e51",
+    "url": "/static/css/main.9ef74093.chunk.css"
+  },
+  {
+    "revision": "5c07a574d2a392e2560d",
+    "url": "/static/js/2.5b872a3d.chunk.js"
+  },
+  {
+    "revision": "bd96a1cfc47c001e68467155960da745",
+    "url": "/static/js/2.5b872a3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ef7b5592a0842813e51",
+    "url": "/static/js/main.754f9ee0.chunk.js"
+  },
+  {
+    "revision": "3ac53baf4f4abda93296",
+    "url": "/static/js/runtime-main.e8383adf.js"
+  },
+  {
+    "revision": "84afe6a1454191fa3083e6702b84a7c5",
+    "url": "/static/media/23328.84afe6a1.jpg"
+  },
+  {
+    "revision": "aaba25d84665a03fbc9e7130c802b67e",
+    "url": "/static/media/alan-2.aaba25d8.jpeg"
+  }
+]);
